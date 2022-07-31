a6fadea90cc06cc585b424e2bfbaee165ef2c2b0
ferni232 <fernandorudnevich.inedita@gmail.com>
2022-07-31 17:08:22 -0300

    repo socket chat actualizado

---
+++ public/main.js
@@ -0,0 +1,39 @@
+
+const socket = io.connect();
+
+
+
+function render(data) {
+    const html = data.map((elem, index) => {
+        return(`<div>
+            <strong>${elem.author}</strong>:
+            <em>${elem.text}</em> </div>`)
+    }).join(" ");
+    document.getElementById('messages').innerHTML = html;
+    document.getElementById('texto').value = "";
+}
+
+socket.on('messages', function(data) { render(data); });
+
+//recibimos mensaje del cliente
+
+function addMessage(e) {
+    const mensaje = {
+        author: document.getElementById('username').value,
+        text: document.getElementById('texto').value
+    };
+    socket.emit('new-message', mensaje);
+    return false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
